266612e68553781bf348cee0b3e7b8a70e4f299b
Yosef Ting <167012861+DJ-Yosef@users.noreply.github.com>
2024-09-24 23:39:12 +0800

    Update class_achieve.py

--- poker/class_achieve.py
+++ poker/class_achieve.py
@@ -1,5 +1,19 @@
 import random
 for cardfunction import *
+
+"""在纸牌游戏中，需要一整副牌（a deck of cards），
+而一摞牌叫stack，一个玩家（player）手上的牌叫一手（hand）牌。
+纸牌正面（face/front）有不同的数字或字母和图案（motif），
+背面（back）相同。在随机洗牌（randomized shuffling）时，
+可以正面朝上（face up），也可以正面朝下（face down）。
+给每个玩家分配纸牌时，叫发牌（deal）。发牌的人叫dealer，
+banker是庄家，切牌叫cut the deck，开扇叫fan，摊牌用spread，
+pass是移牌，force迫牌，riffle拨牌，bottom deal发底牌，
+central deal发中间牌，palm是藏牌，top palm藏顶牌，
+to stake下赌注，to raise加赌注，chip是筹码，in跟牌下注，
+out不跟，to bid叫牌，犯规是infraction/break the rules。
+"""
+
 class player:
     '''游戏玩家'''
     num=1
